3ea4c28cd1d3e9c068a14326d99047cdf2cf0481
Mzwandile <mzwandilethatojunior@gmail.com>
2023-11-03 02:22:27 +0200

    project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  // function historyChecker() {
+  //   let visitedUrls = window.history;
 
-function App() {
-  const [count, setCount] = useState(0)
+  //   for (let element in history) {
+  //     let site = document.createElement("p");
+  //     site.innerHTML = visitedUrls[element];
+  //     document.getElementById("historyList").appendChild(site);
+  //   }
+  // }
+
+  function historyChecker2() {
+    let urls = document.getElementsByTagName("a");
+
+    for (let url in urls) {
+      document.write(urls[url].href);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>History checker</h1>
+      <h2>The previous sites visited are: </h2>
+      <button onClick={historyChecker2}>Show history</button>
+      <div id="historyList"></div>
+    </div>
+  );
+};
 
-export default App
+export default App;
